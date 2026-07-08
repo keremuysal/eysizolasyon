bc0790542225c080a62fd503b2f6c214de5c2890
Kerem Uysal <info.keremuysal@gmail.com>
2026-07-08 19:47:18 +0300

    yolları düzelttim

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/eysizolasyon",
-  assetPrefix: "/eysizolasyon/", // BU SATIR ÇOK ÖNEMLİ
   images: {
     unoptimized: true,
   },
 };
-
-const basePath = "/eysizolasyon";
-export const getPath = (path: string) => `${basePath}${path}`;
-
-// Kullanımı:
-<img src={getPath("/images/eys-logo.png")} />
 
 export default nextConfig;
